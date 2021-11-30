6c068c3bc09d824e8cd9a89d219b80be9f76341d
Lance <lance.chandler@dmail.dixie.edu>
2021-11-30 11:44:20 -0700

    who does what

--- lance.py
+++ lance.py
@@ -9,16 +9,21 @@ class Final():
         data = pd.read_csv("whr happiness.csv")
         self.df = pd.DataFrame(data)
     
+    #Lance
     def graphical_sum(self):
         pass
+    #Ethan
     def numerical_sum(self):
         pass
+    #Lance
     def normal_dis(self):
         pass
+    #Ethan
     def hypothesis_tests(self):
         pass
     def ANOVA(self):
         #if possible]
         pass
+    #Lance
     def linear_regression(self):
         pass
