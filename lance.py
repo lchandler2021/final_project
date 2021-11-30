4800223fd9e16b409f463dfcb6a62f30e08a7dfd
Lance <lance.chandler@dmail.dixie.edu>
2021-11-30 11:41:13 -0700

    setting up our class

---
+++ lance.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+
+
+class Final():
+    
+    def __init__(self):
+    
+        data = pd.read_csv("whr happiness.csv")
+        self.df = pd.DataFrame(data)
+    
+    def graphical_sum(self):
+        pass
+    def numerical_sum(self):
+        pass
+    def normal_dis(self):
+        pass
+    def hypothesis_tests(self):
+        pass
+    def ANOVA(self):
+        #if possible]
+        pass
+    def linear_regression(self):
+        pass
