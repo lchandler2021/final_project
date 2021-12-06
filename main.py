9951b0f1e3067477e71491995553148866e806f2
Lance Chandler <89875929+lchandler2021@users.noreply.github.com>
2021-12-05 19:33:49 -0700

    Update main.py
    adding to main for ethan

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-import lancedef main():    lance.Final().graphical_sum()    lance.Final().normal_dis()    lance.Final().linear_regression()        main()
+import lance
+
+def main():
+    lance.Final().graphical_sum()
+    lance.Final().normal_dis()
+    lance.Final().linear_regression()
+    lance.Final().main()
+    
+main()
