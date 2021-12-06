77c8f41ad63522e8070fd4f9f0b4193e9778befd
Lance <lance.chandler@dmail.dixie.edu>
2021-12-05 18:52:19 -0700

    Lance's part

--- lance.py
+++ lance.py
@@ -1,5 +1,10 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
+import statistics
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
 
 
 class Final():
@@ -11,13 +16,44 @@ class Final():
     
     #Lance
     def graphical_sum(self):
-        pass
+        #print(self.df)
+        plt.scatter(self.df["Rank by 2020 score"], self.df["Rank by 2017-19 score"])
+        plt.xlabel("Rank by 2020 Score")
+        plt.ylabel("Rank by 2017-19 Score")
+        plt.show()
+        plt.close()
+        #Scatter plot showing how the happiest countries have imporved or gotten less happy.
+        
     #Ethan
     def numerical_sum(self):
         pass
     #Lance
+    def numstdev(self,data,number):
+        set_size = len(data)
+        mean  = statistics.mean(data)
+        stdev = statistics.stdev(data)
+        stdevbelow = mean - stdev*(number)
+        stdevabove = mean + stdev*(number)
+        new_set = []
+        #print(set_size)
+        #print(stdev)
+        for num in data:
+            #print(num)
+            if stdevbelow <= num and stdevabove >= num:
+                new_set.append(num)
+        return len(new_set)/set_size
+    
     def normal_dis(self):
-        pass
+        print("="*20)
+        print("Normal Distrobution")
+        print("Scores 2020")
+        print("Percent within one standard deviation of mean :: ", self.numstdev(self.df["Score, 2020"],1))
+        print("Percent within two standard deviation of mean :: ", self.numstdev(self.df["Score, 2020"],2))
+        print("Percent within three standard deviation of mean :: ", self.numstdev(self.df["Score, 2020"],3))
+        print("Scores 2017-2019")
+        print("Percent within one standard deviation of mean :: ", self.numstdev(self.df["Score_2017-19"],1))
+        print("Percent within two standard deviation of mean :: ", self.numstdev(self.df["Score_2017-19"],2))
+        print("Percent within three standard deviation of mean :: ", self.numstdev(self.df["Score_2017-19"],3))
     #Ethan
     def hypothesis_tests(self):
         pass
@@ -26,4 +62,29 @@ class Final():
         pass
     #Lance
     def linear_regression(self):
-        pass
+        print("="*20)
+        print("Linear Regression")
+        x = self.df[["Score, 2020","Rank by 2017-19 score"]]
+        #z = self.df["Rank by 2017-19 score"]
+        #print(x,z)
+
+    
+        y = self.df["Score_2017-19"]
+        x_train,x_test,y_train,y_test = train_test_split(x,y,train_size = 0.3,random_state=100)
+        
+        mlr = LinearRegression()
+        mlr.fit(x_train,y_train)
+        
+        y_pred_mlr = mlr.predict(x_test)
+        mlr_dif = pd.DataFrame({"2017-19 scores":y_test,"2020 scores":y_pred_mlr})
+        print(mlr_dif)
+        meanAbsError = metrics.mean_absolute_error(y_test,y_pred_mlr)
+        meanSqError = metrics.mean_squared_error(y_test,y_pred_mlr)
+        rootMeanSqError = np.sqrt(metrics.mean_squared_error(y_test, y_pred_mlr))
+        
+        print("R Squared {:.2f}".format(mlr.score(x,y)*100))
+        print("Mean Absolute Error:: ",meanAbsError)
+        print("Mean Square Error:: ",meanSqError)
+        print("Root Mean Square error::",rootMeanSqError)
+   
+    
---
+++ main.py
@@ -0,0 +1,1 @@
+import lancedef main():    lance.Final().graphical_sum()    lance.Final().normal_dis()    lance.Final().linear_regression()        main()
