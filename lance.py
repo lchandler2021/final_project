af01d58f09af089a047bfabcb89ece6686dfa400
Lance Chandler <89875929+lchandler2021@users.noreply.github.com>
2021-12-05 19:37:49 -0700

    Update lance.py
    fixed import

--- lance.py
+++ lance.py
@@ -5,7 +5,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn import metrics
-import scipy as sc
+import scipy.stats as sc
 
 
 class Final():
